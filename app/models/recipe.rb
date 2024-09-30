ce598136b02303e610dd57a3c0ed3f503ad3fd18
Marcelo Mancini <mancini2007@gmail.com>
2024-09-30 12:35:46 +0100

    Add Recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,8 @@
 class Recipe < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
-  validates :description, presence: true
-  validates :image_url, presence: true, url: true
-  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
-
   has_many :bookmarks
   has_many :categories, through: :bookmarks
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  # validates :image_url, presence: true, url: true
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,4 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Recipe.create(name: "Spaghetti Carbonara", description: "A classic Italian pasta dish with egg, cheese, and black pepper.", rating: 9.0)
+Recipe.create(name: "Pesto Genovese", description: "A popular Indian curry dish with marinated chicken and a creamy tomato sauce.", rating: 8.0)
+Recipe.create(name: "Chicken Tikka Masala", description: "A classic Italian pasta dish with egg, cheese, and black pepper.", rating: 8.5)
+Recipe.create(name: "Sushi Platter", description: "A variety of raw fish and rice rolls, a classic Japanese dish", rating: 7.5)
